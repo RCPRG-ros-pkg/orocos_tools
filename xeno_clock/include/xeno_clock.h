0d41821bba938dc27b75c4c4c20437db6802f129
Konrad Banachowicz <konradb3@gmail.com>
2013-03-27 13:12:02 +0100

    fix gnulinux build.

--- xeno_clock/include/xeno_clock.h
+++ xeno_clock/include/xeno_clock.h
@@ -1,4 +1,6 @@
 ros::Time now() {
+
+#if OROCOS_TARGET_XENOMAI == 1
   ros::Time t;
   
   timespec start;
@@ -6,4 +8,9 @@ ros::Time now() {
   t.sec  = start.tv_sec;
   t.nsec = start.tv_nsec;
   return t;
+#elif OROCOS_TARGET_GNULINUX == 1
+  return ros::Time::now();
+#else
+  #error "unsupported OROCOS_TARGET"
+#endif
 }
